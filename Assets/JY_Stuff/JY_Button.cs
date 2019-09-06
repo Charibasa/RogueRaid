8a830d6d7ecf3e6b422306bcf7a9930ddbb8244a
Charibasa <jyu1@ggc.edu>
2019-09-06 18:56:05 -0400

    Initial commit

---
+++ Assets/JY_Stuff/JY_Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JY_Button : MonoBehaviour
+{
+    public Sprite Up;
+    public Sprite Right;
+    public Sprite Down;
+    public Sprite Left;
+    public int Dir;
+    private Image SR;
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        SR = GetComponent<Image>();
+
+        switch (Dir)
+        {
+            case (1):
+                SR.sprite = Up;
+                break;
+            case (2):
+                SR.sprite = Right;
+                break;
+            case (3):
+                SR.sprite = Down;
+                break;
+            case (4):
+                SR.sprite = Left;
+                break;
+        }
+    }
+}
---
+++ Assets/JY_Stuff/JY_Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JY_Move : MonoBehaviour
+{
+    private bool m_CanMove;
+
+    public bool CanMove
+    {
+        get
+        {
+            return m_CanMove;
+        }
+
+        set
+        {
+            m_CanMove = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 m = new Vector3(h, v, 0);
+
+        Vector3 t = transform.position + m * Time.deltaTime * 5;
+
+        if(CanMove)
+        {
+            transform.position = t;
+        }
+    }
+}
