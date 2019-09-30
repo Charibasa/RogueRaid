5d1392a6211ef2ed6e8b94e7943fe04b2326ea98
Joseph Zoretic <jzoretic95@gmail.com>
2019-09-30 12:37:34 -0400

    traps base script setup

--- Assets/JZ_Stuff/Scripts/JZ_Timer.cs
+++ Assets/JZ_Stuff/Scripts/JZ_Timer.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class JZ_Timer : MonoBehaviour
 {
-    public float timeReduction = 0f;
-    public float timeAddition = 0f;
+    // public float timeReduction = 0f;
+    // public float timeAddition = 0f;
     public float roundTime = 2f;
-    private float countdown = 2f;
+    public float countdown = 2f;
     public GameManager GM;
 
     public Text roundTimerTxt;
     void Start()
     {
-        
+        countdown = roundTime;  
     }
 
     // Update is called once per frame
@@ -24,10 +24,17 @@ public class JZ_Timer : MonoBehaviour
         {
             //end game
         }
-        countdown -= timeReduction;
-        countdown += timeAddition;
+        // countdown -= timeReduction;
+        // countdown += timeAddition;
         countdown -= Time.deltaTime;
         countdown = Mathf.Clamp(countdown,0f,Mathf.Infinity);
-        roundTimerTxt.text = string.Format("{00:00.00}", countdown);
+        roundTimerTxt.text = string.Format("{0:00.0}", countdown);
+    }
+    public float addtime(float timeAdding) {
+        return countdown + timeAdding;
+    }
+    public float reducetime(float timeReducing) {
+        
+        return countdown - timeReducing;
     }
 }
